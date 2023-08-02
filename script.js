45d527973ff1eacc5c365bceb4d0bcd7cf190af3
NyeishaJPace <nyeisha.j.pace@gmail.com>
2023-08-02 22:34:48 +0000

    cleaning up files

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-/* var qrcode = new
-QRCode(document.querySelector(".qrcode"));
-qrcode.makeCode("Why did you scanned me?")
-function generateQR(){
-	qrcode.makeCode(document.querySelector("input").value)
-} 
-
-var QRCode = require('qrcode')
-var canvas = document.getElementById('canvas')
-
-QRCode.toCanvas(canvas, 'sample text', function (error) {
-  if (error) console.error(error)
-  console.log('success!');
-}) */
 
 document.getElementById('generateBtn').addEventListener('click', function () {
     var textInput = document.getElementById('textInput').value; // Get the text input value
