232f8e73449e83227a0ee47942233b15d0c34338
NyeishaJPace <nyeisha.j.pace@gmail.com>
2023-08-02 21:31:45 +0000

    installed qrcode.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-var qrcode = new
+/* var qrcode = new
 QRCode(document.querySelector(".qrcode"));
 qrcode.makeCode("Why did you scanned me?")
 function generateQR(){
 	qrcode.makeCode(document.querySelector("input").value)
-}
+} */
+
+var QRCode = require('qrcode')
+var canvas = document.getElementById('canvas')
+
+QRCode.toCanvas(canvas, 'sample text', function (error) {
+  if (error) console.error(error)
+  console.log('success!');
+})
