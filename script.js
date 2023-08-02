2fc110e78c2f323d50964b98af86878dbbf0c18c
NyeishaJPace <nyeisha.j.pace@gmail.com>
2023-08-02 21:46:49 +0000

    updating qrcode file

--- script.js
+++ script.js
@@ -17,11 +17,19 @@ document.getElementById('generateBtn').addEventListener('click', function () {
     var QRCode = require('qrcode'); // You don't need to require again, just use the existing QRCode object.
   
     var textInput = document.getElementById('textInput').value; // Get the text input value
-    var canvas = document.getElementById('qrcode'); // Get the canvas element
+    var qrCodeContainer = document.getElementById('qrcode'); // Get the div container
   
-    QRCode.toCanvas(canvas, textInput, function (error) { // Generate the QR code with the input text
-      if (error) console.error(error);
-      console.log('success!');
+    QRCode.toDataURL(textInput, function (error, url) { // Generate the QR code data URL with the input text
+      if (error) {
+        console.error(error);
+      } else {
+        var qrCodeImage = document.createElement('img'); // Create an <img> element
+        qrCodeImage.src = url; // Set the src attribute to the QR code data URL
+        qrCodeContainer.innerHTML = ''; // Clear the container in case there's already a QR code
+        qrCodeContainer.appendChild(qrCodeImage); // Append the QR code image to the container
+        console.log('success!');
+      }
     });
   });
+  
   
