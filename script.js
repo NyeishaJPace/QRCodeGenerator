416439bd5651035b811b0c52719a8e8bc561741a
NyeishaJPace <nyeisha.j.pace@gmail.com>
2023-08-02 21:42:20 +0000

    added cdn script

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ QRCode(document.querySelector(".qrcode"));
 qrcode.makeCode("Why did you scanned me?")
 function generateQR(){
 	qrcode.makeCode(document.querySelector("input").value)
-} */
+} 
 
 var QRCode = require('qrcode')
 var canvas = document.getElementById('canvas')
@@ -11,4 +11,17 @@ var canvas = document.getElementById('canvas')
 QRCode.toCanvas(canvas, 'sample text', function (error) {
   if (error) console.error(error)
   console.log('success!');
-})
+}) */
+
+document.getElementById('generateBtn').addEventListener('click', function () {
+    var QRCode = require('qrcode'); // You don't need to require again, just use the existing QRCode object.
+  
+    var textInput = document.getElementById('textInput').value; // Get the text input value
+    var canvas = document.getElementById('qrcode'); // Get the canvas element
+  
+    QRCode.toCanvas(canvas, textInput, function (error) { // Generate the QR code with the input text
+      if (error) console.error(error);
+      console.log('success!');
+    });
+  });
+  
